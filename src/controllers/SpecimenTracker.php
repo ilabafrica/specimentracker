f8db2cb2a1aefbb3240efe383c764dd84fd5fc88
13enjamin <benjaminodonya@gmail.com>
2018-11-20 16:01:32 +0300

    SQL Query fix: use eloquent + Raw

---
+++ src/controllers/SpecimenTracker.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ILabAfrica\SpecimenTracker\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use DB;
+
+class SpecimenTracker extends Controller
+{
+    //
+      /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public static function identifier()
+    {
+        $where ="MONTH(`created_at`) =".date('m')." AND YEAR(`created_at`)=".date('Y');
+        $count =  DB::table('specimens')->whereRaw($where)->count();
+        $count+=1;
+        
+        $tracker =date('Y_m_d');
+
+        $data = 'ILAB_'.$count. '_'.$tracker;
+
+        return $data;
+
+    }
+}
