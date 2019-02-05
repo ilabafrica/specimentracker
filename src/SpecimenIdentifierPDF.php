1abc8b2629d0834d0a5cc5cc242334acbc85c371
13enjamin <benjaminodonya@gmail.com>
2019-02-05 15:16:32 +0300

    print specimen identifier as qr code

--- src/SpecimenIdentifierPDF.php
+++ src/SpecimenIdentifierPDF.php
@@ -11,13 +11,13 @@ class SpecimenIdentifierPDF extends TCPDF
     {
         if ($this->page == 1) {
             // Logo
-            $image_file = 'ilabafrica.jpg';
-            $this->Image($image_file, 75, 10, 50, '', 'JPG', '', '', false, 300, '', false, false, 0, false, false, false);
+            /*$image_file = 'ilabafrica.jpg';
+            $this->Image($image_file, 75, 10, 50, '', 'JPG', '', '', false, 300, '', false, false, 0, false, false, false);*/
             $this->ln();
             // Set font
             $this->SetFont('helvetica', 'B', 20);
 
-            $this->SetMargins(PDF_MARGIN_LEFT, 50, PDF_MARGIN_RIGHT);
+            $this->SetMargins(PDF_MARGIN_LEFT, 5, PDF_MARGIN_RIGHT);
         } else {
             $this->SetMargins(PDF_MARGIN_LEFT, 15, PDF_MARGIN_RIGHT);
         }
@@ -26,7 +26,7 @@ class SpecimenIdentifierPDF extends TCPDF
     public function Footer()
     {
         // Class 'App\Models\DateTime' not found
-        // $now = new DateTime();
+        $now = date('Y-m-d H:i');
         // $printTime = $now->format('Y-m-d H:i');
 
         //Position at 15mm at the bottom
@@ -34,7 +34,7 @@ class SpecimenIdentifierPDF extends TCPDF
         //Set font
         $this->SetFont('helvetica', 'I', 8);
         //set page number
-        $this->Cell(0, 10, 'Page '.$this->getAliasNumPage().'/'.$this->getAliasNbPages(), 0, false, 'L', 0, '', 0, false, 'T', 'M');
+        $this->Cell(0, 10, 'As of '.$now, 0, false, 'L', 0, '', 0, false, 'T', 'M');
         // $this->Cell(0, 10, "Printed by: ".Auth::user()->name." Date: ".$printTime, 0, false, 'R', 0, '', 0, false, 'T', 'M');
     }
 }
--- src/controllers/SpecimenTracker.php
+++ src/controllers/SpecimenTracker.php
@@ -21,7 +21,7 @@ class SpecimenTracker extends Controller
     {
         $where ="MONTH(`created_at`) =".date('m')." AND YEAR(`created_at`)=".date('Y');
         $count =  DB::table('specimens')->whereRaw($where)->count();
-        $count+=1;
+        $count +=1;
 
         $tracker =date('Y_m_d');
 
@@ -36,14 +36,31 @@ class SpecimenTracker extends Controller
         $specimen = Specimen::whereId($id)->first();
         $identifier = $specimen->identifier;
 
-        $pdf = new SpecimenIdentifierPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+        $custom_layout = array(70, 70);
+        $pdf = new SpecimenIdentifierPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, $custom_layout, true, 'UTF-8', false);
 
+        // set image scale factor
+        $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+        
+        // set style for qr code
+        $style = array(
+            'border' => 2,
+            'padding' => 'auto',
+            'fgcolor' => array(0,0,0),
+            'bgcolor' => false
+        );
+
+        // QRCODE,H : QR-CODE Best error correction
+        $pdf->write2DBarcode($identifier, 'QRCODE,H', 5, 50, 50, 50, $style, 'N');
         //use a custom view to make the html content to be used in generation of the pdf
         //this approach allows for more html like styling though limited
-        $view = view('specimentracker::pdf', compact('identifier'));
+        
+        //print Text
+        // $pdf->AddPage();
+        /*$view = view('specimentracker::pdf', compact('identifier'));
         $html_content = $view->render();
-        $pdf->AddPage();
-        $pdf->writeHTML($html_content, true, false, true, false, '');
+        
+        $pdf->writeHTML($html_content, true, false, true, false, '');*/
 
         return $pdf->Output('example_001.pdf', 'I');
     }
